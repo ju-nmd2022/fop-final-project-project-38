cf1534614f7dd7939f65fe77251f2b7f9eb2502e
Lucas <rolu22fh@student.ju.se>
2023-04-27 17:59:26 +0200

    a

--- tetris.js
+++ tetris.js
@@ -1,26 +1,25 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+window.onload = function() {
+    setup();
+};
 
 //grid
 const ROWS = 20;
 const COLS = 10;
 const BLOCK_SIZE = 20;
 
-canvas.width = COLS * BLOCK_SIZE;
-canvas.height = ROWS * BLOCK_SIZE;
-
-// draw the grid
-ctx.strokeStyle = '#000';
-ctx.lineWidth = 1;
-for (let i = 0; i <= COLS; i++) {
-	ctx.beginPath();
-	ctx.moveTo(i * BLOCK_SIZE, 0);
-	ctx.lineTo(i * BLOCK_SIZE, canvas.height);
-	ctx.stroke();
+function setup() {
+    const canvas = createCanvas(COLS * BLOCK_SIZE, ROWS * BLOCK_SIZE);
+    canvas.parent('canvas-container');
 }
-for (let i = 0; i <= ROWS; i++) {
-	ctx.beginPath();
-	ctx.moveTo(0, i * BLOCK_SIZE);
-	ctx.lineTo(canvas.width, i * BLOCK_SIZE);
-	ctx.stroke();
+
+function draw() {
+    background(255);
+    stroke(0);
+    strokeWeight(1);
+    for (let i = 0; i <= COLS; i++) {
+	    line(i * BLOCK_SIZE, 0, i * BLOCK_SIZE, height);
+    }
+    for (let i = 0; i <= ROWS; i++) {
+	    line(0, i * BLOCK_SIZE, width, i * BLOCK_SIZE);
+    }
 }
