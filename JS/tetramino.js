04339d5b53a112bcb277cb683cafa16f21670d0e
Lucas <rolu22fh@student.ju.se>
2023-05-21 16:21:13 +0200

    a

---
+++ JS/tetramino.js
@@ -0,0 +1,18 @@
+class Tetramino {
+    constructor(shape, color) {
+      this.shape = shape;
+      this.color = color;
+      this.sprite = createSprite();
+      this.sprite.addAnimation('default', shape);
+      this.sprite.scale = BLOCK_SIZE / max(shape.width, shape.height);
+    }
+  
+    setPosition(x, y) {
+      this.sprite.position.x = x;
+      this.sprite.position.y = y;
+    }
+  
+    display() {
+      this.sprite.display();
+    }
+  }
--- tetris.js
+++ tetris.js
@@ -1,7 +1,3 @@
-window.onload = function() {
-    setup();
-};
-
 //grid
 const ROWS = 30;
 const COLS = 15;
@@ -23,3 +19,12 @@ function draw() {
 	    line(0, i * BLOCK_SIZE, width, i * BLOCK_SIZE);
     }
 }
+
+// Display score
+fill(0);
+textAlign(RIGHT, TOP);
+textSize(24);
+text(`Score: ${score}`, width - 180, 40);
+text(`Level: ${level}`, width - 180, 80);
+text('Next Tetramino:', width - 180, 120);
+displayTetramino(nextTetramino, COLS + 2, 10);
