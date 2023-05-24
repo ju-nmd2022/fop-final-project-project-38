2eecabee823b589987f830c0c797536af1ed6fd2
Lucas <rolu22fh@student.ju.se>
2023-05-25 01:02:29 +0200

    a

--- JS/tetris.js
+++ JS/tetris.js
@@ -11,7 +11,6 @@ const BLOCK_SIZE = 30;
 function setup() {
     const canvas = createCanvas(COLS * BLOCK_SIZE, ROWS * BLOCK_SIZE);
     canvas.parent('canvas-container');
-    
 }
 
 function draw() {
