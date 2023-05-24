c5735137f08b87e2881c0f64e63d3cfe7cc2a7c8
Lucas <rolu22fh@student.ju.se>
2023-05-25 01:22:28 +0200

    a

--- JS/tetris.js
+++ JS/tetris.js
@@ -5,30 +5,37 @@ let score = 0;
 let tetrominoes = [];
 let backgroundImage;
 
-function preload() {
-  backgroundImage = loadImage("../Level1.jpg");
-}
-
 //grid
 const ROWS = 30;
 const COLS = 15;
 const BLOCK_SIZE = 30;
 
+let currentTetromino; 
+
+function preload() {
+  backgroundImage = loadImage("../Level1.jpg");
+}
+
 function setup() {
-    const canvas = createCanvas(COLS * BLOCK_SIZE, ROWS * BLOCK_SIZE);
-    canvas.parent('canvas-container');
+  const canvas = createCanvas(COLS * BLOCK_SIZE, ROWS * BLOCK_SIZE);
+  canvas.parent('canvas-container');
+
+  currentTetromino = new Tetromino();
 }
 
 function draw() {
-    background(0);
-    image(backgroundImage, 0, 0);
-    stroke(0);
-    strokeWeight(1);
-    for (let i = 0; i <= COLS; i++) {
-	    line(i * BLOCK_SIZE, 0, i * BLOCK_SIZE, height);
-    }
-    for (let i = 0; i <= ROWS; i++) {
-	    line(0, i * BLOCK_SIZE, width, i * BLOCK_SIZE);
-    }
-}
+  background(0);
+  image(backgroundImage, 0, 0);
+  stroke(0);
+  strokeWeight(1);
 
+  for (let i = 0; i <= COLS; i++) {
+    line(i * BLOCK_SIZE, 0, i * BLOCK_SIZE, height);
+  }
+  for (let i = 0; i <= ROWS; i++) {
+    line(0, i * BLOCK_SIZE, width, i * BLOCK_SIZE);
+  }
+
+  currentTetromino.update();
+  currentTetromino.display();
+}
