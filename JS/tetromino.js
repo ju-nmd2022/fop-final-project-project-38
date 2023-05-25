e2cf5533321a5cab0c222249d2a70dbd77b5dbb5
Lucas <rolu22fh@student.ju.se>
2023-05-25 09:29:39 +0200

    a

--- JS/tetromino.js
+++ JS/tetromino.js
@@ -1,66 +1,65 @@
 class Tetromino {
   constructor() {
-      this.row = 0; // Initial row position
-      this.col = Math.floor(COLS / 2); // Initial column position
-      this.color = this.getRandomColor(); 
-      this.shape = this.getRandomShape();
+    this.row = 0; // Initial row position
+    this.col = Math.floor(COLS / 2); // Initial column position
+    this.color = this.getRandomColor(); 
+    this.shape = this.getRandomShape();
   }
 
   show() {
-      for (let row = 0; row < this.shape.length; row++) {
-          for (let col = 0; col < this.shape[row].length; col++) {
-              if (this.shape[row][col] === 1) {
-                  fill(this.color);
-                  rect(
-                      (this.col + col) * BLOCK_SIZE,
-                      (this.row + row) * BLOCK_SIZE,
-                      BLOCK_SIZE,
-                      BLOCK_SIZE
-                  );
-              }
-          }
+    for (let row = 0; row < this.shape.length; row++) {
+      for (let col = 0; col < this.shape[row].length; col++) {
+        if (this.shape[row][col] === 1) {
+          fill(this.color);
+          rect(
+            (this.col + col) * BLOCK_SIZE,
+            (this.row + row) * BLOCK_SIZE,
+            BLOCK_SIZE,
+            BLOCK_SIZE
+          );
+        }
       }
+    }
   }
 
   update() {
   }
 
   moveDown() {
-      this.row++;
+    this.row++;
   }
 
   moveLeft() {
-      this.col--;
+    this.col--;
   }
 
   moveRight() {
-      this.col++;
+    this.col++;
   }
 
   rotate() {
-      const rotatedShape = [];
-      for (let col = 0; col < this.shape[0].length; col++) {
-          const newRow = [];
-          for (let row = this.shape.length - 1; row >= 0; row--) {
-              newRow.push(this.shape[row][col]);
-          }
-          rotatedShape.push(newRow);
+    const rotatedShape = [];
+    for (let col = 0; col < this.shape[0].length; col++) {
+      const newRow = [];
+      for (let row = this.shape.length - 1; row >= 0; row--) {
+        newRow.push(this.shape[row][col]);
       }
-      this.shape = rotatedShape;
+      rotatedShape.push(newRow);
+    }
+    this.shape = rotatedShape;
   }
 
   getRandomShape() {
-      const shapes = [
-          [[1, 1, 1, 1]], 
-          [[1, 1], [1, 1]], 
-          [[1, 1, 1], [0, 1, 0]],
-      ];
-      return random(shapes);
+    const shapes = [
+      [[1, 1, 1, 1]], 
+      [[1, 1], [1, 1]], 
+      [[1, 1, 1], [0, 1, 0]],
+    ];
+    return random(shapes);
   }
 
   getRandomColor() {
-    const randomColor = () => Math.floor(Math.random() * 256);
-    return color(randomColor(), randomColor(), randomColor()); 
+    return color(random(255), random(255), random(255));
   }
 
   canMoveDown() {
@@ -78,6 +77,6 @@ class Tetromino {
         }
       }
     }
-    return true;
+    return true; 
   }
 }
