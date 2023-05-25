a69867a9e879d8f009600143183c9595c2b123da
Lucas <rolu22fh@student.ju.se>
2023-05-25 21:03:39 +0200

    a

--- JS/tetris.js
+++ JS/tetris.js
@@ -124,7 +124,7 @@ class GameModel {
     }
 
     if (this.fallingPiece !== null) {
-      this.fallingPiece.show();
+      this.fallingPiece.renderPiece();
     }
   }
 
@@ -195,5 +195,29 @@ class GameModel {
       }
     }
     this.renderGameState();
+  }
+}
+
+class Piece {
+  constructor(shape) {
+    this.shape = shape;
+    this.y = 0;
+    this.x = Math.floor(COLS / 2);
+  }
+
+  renderPiece() {
+    for (let i = 0; i < this.shape.length; i++) {
+      for (let j = 0; j < this.shape[i].length; j++) {
+        if (this.shape[i][j] > 0) {
+          fill(COLORS[this.shape[i][j]]);
+          rect(
+            (this.x + j) * BLOCK_SIZE,
+            (this.y + i) * BLOCK_SIZE,
+            BLOCK_SIZE,
+            BLOCK_SIZE
+          );
+        }
+      }
+    }
   }
 }
