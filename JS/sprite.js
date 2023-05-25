b4577ef43ef90060308abad11d98f912bd219f9d
Lucas <rolu22fh@student.ju.se>
2023-05-25 02:30:16 +0200

    a

---
+++ JS/sprite.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext ('2d')
+
+class Sprite {
+    constructor (position) 
+    {
+        this.position = position 
+    }
+}
