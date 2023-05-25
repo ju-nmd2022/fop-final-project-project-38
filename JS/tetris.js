9b8c970decbe62fc2e5d4fc455e949ab54644fab
Lucas <rolu22fh@student.ju.se>
2023-05-25 02:55:52 +0200

    a

--- JS/tetris.js
+++ JS/tetris.js
@@ -3,14 +3,16 @@ const COLS = 15;
 const BLOCK_SIZE = 30;
 let tetrominoes = [];
 let backgroundImage;
+let canvasContext;
 
 function preload() {
     backgroundImage = loadImage("../IMG/Level1.jpg");
-  }
+}
 
 function setup() {
     const canvas = createCanvas(COLS * BLOCK_SIZE, ROWS * BLOCK_SIZE);
     canvas.parent('canvas-container');
+    canvasContext = canvas.canvas.getContext('2d');
 }
 
 function draw() {
@@ -18,10 +20,14 @@ function draw() {
     image(backgroundImage, 0, 0);
     stroke(0);
     strokeWeight(1);
+    canvasContext.beginPath();
     for (let i = 0; i <= COLS; i++) {
-	    line(i * BLOCK_SIZE, 0, i * BLOCK_SIZE, height);
+        canvasContext.moveTo(i * BLOCK_SIZE, 0);
+        canvasContext.lineTo(i * BLOCK_SIZE, height);
     }
     for (let i = 0; i <= ROWS; i++) {
-	    line(0, i * BLOCK_SIZE, width, i * BLOCK_SIZE);
+        canvasContext.moveTo(0, i * BLOCK_SIZE);
+        canvasContext.lineTo(width, i * BLOCK_SIZE);
     }
+    canvasContext.stroke();
 }
