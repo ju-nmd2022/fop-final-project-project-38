73a1a77f15febcb964fde97af19e0e1ed1c9f686
Lucas <rolu22fh@student.ju.se>
2023-05-25 16:21:47 +0200

    a

--- JS/tetromino.js
+++ JS/tetromino.js
@@ -1,10 +1,9 @@
 class Tetromino {
-  constructor(tetrominoes) {
+  constructor() {
     this.row = 0; // Initial row position
     this.col = Math.floor(COLS / 2); // Initial column position
-    this.color = this.getRandomColor();
+    this.color = this.getRandomColor(); 
     this.shape = this.getRandomShape();
-    this.tetrominoes = tetrominoes;
   }
 
   show() {
@@ -24,37 +23,18 @@ class Tetromino {
   }
 
   update() {
-    
   }
 
   moveDown() {
-    if (this.canMoveDown()) {
-      this.row++;
-    } else {
-      // Tetromino has reached the bottom or collided with another tetromino
-      // Add the current tetromino to the tetrominoes array
-      for (let row = 0; row < this.shape.length; row++) {
-        for (let col = 0; col < this.shape[row].length; col++) {
-          if (this.shape[row][col] === 1) {
-            tetrominoes[this.row + row][this.col + col] = this.color;
-          }
-        }
-      }
-      // Create a new tetromino
-      tetromino = new Tetromino();
-    }
+    this.row++;
   }
 
   moveLeft() {
-    if (this.canMoveLeft()) {
-      this.col--;
-    }
+    this.col--;
   }
 
   moveRight() {
-    if (this.canMoveRight()) {
-      this.col++;
-    }
+    this.col++;
   }
 
   rotate() {
@@ -66,28 +46,22 @@ class Tetromino {
       }
       rotatedShape.push(newRow);
     }
-    if (this.canRotate(rotatedShape)) {
-      this.shape = rotatedShape;
-    }
+    this.shape = rotatedShape;
   }
 
   getRandomShape() {
     const shapes = [
-      [[1, 1, 1, 1]],
-      [[1, 1], [1, 1]],
+      [[1, 1, 1, 1]], 
+      [[1, 1], [1, 1]], 
       [[1, 1, 1], [0, 1, 0]],
-      [[1, 1, 0], [0, 1, 1]],
-      [[0, 1, 1], [1, 1, 0]],
-      [[1, 1, 1], [1, 0, 0]],
-      [[1, 1, 1], [0, 0, 1]],
     ];
     return random(shapes);
   }
 
   getRandomColor() {
-    return color(random(255), random(255), random(255));
+    return color(random(255), random(255), random(255)); // Generate a random color
   }
-  
+
   canMoveDown() {
     for (let row = 0; row < this.shape.length; row++) {
       for (let col = 0; col < this.shape[row].length; col++) {
@@ -98,81 +72,11 @@ class Tetromino {
             nextRow >= ROWS || // Out of bounds
             (nextRow < ROWS && tetrominoes[nextRow][this.col + col]) // Occupied by another tetromino
           ) {
-            return false; 
+            return false; // Cannot move down
           }
         }
       }
     }
-    return true; 
-  }
-  moveDown() {
-    if (this.canMoveDown()) {
-      this.row++;
-    } else {
-      for (let row = 0; row < this.shape.length; row++) {
-        for (let col = 0; col < this.shape[row].length; col++) {
-          if (this.shape[row][col] === 1) {
-            tetrominoes[this.row + row][this.col + col] = this.color;
-          }
-        }
-      }
-      tetromino = new Tetromino();
-    }
-  }
-
-  canMoveLeft() {
-    for (let row = 0; row < this.shape.length; row++) {
-      for (let col = 0; col < this.shape[row].length; col++) {
-        if (this.shape[row][col] === 1) {
-          const nextCol = this.col + col - 1;
-          // 
-          if (
-            nextCol < 0 || 
-            (nextCol >= 0 && tetrominoes[this.row + row][nextCol]) 
-          ) {
-            return false;
-          }
-        }
-      }
-    }
-    return true;
-  }
-
-  canMoveRight() {
-    for (let row = 0; row < this.shape.length; row++) {
-      for (let col = 0; col < this.shape[row].length; col++) {
-        if (this.shape[row][col] === 1) {
-          const nextCol = this.col + col + 1;
-          if (
-            nextCol >= COLS || 
-            (nextCol < COLS && tetrominoes[this.row + row][nextCol]) 
-          ) {
-            return false;
-          }
-        }
-      }
-    }
-    return true;
-  }
-
-  canRotate(rotatedShape) {
-    for (let row = 0; row < rotatedShape.length; row++) {
-      for (let col = 0; col < rotatedShape[row].length; col++) {
-        if (rotatedShape[row][col] === 1) {
-          const newRow = this.row + row;
-          const newCol = this.col + col;
-          if (
-            newRow < 0 || 
-            newRow >= ROWS || 
-            newCol < 0 || 
-            newCol >= COLS || 
-            tetrominoes[newRow][newCol] 
-          ) {
-            return false;
-          }
-        }
-      }
-    }
-    return true; 
+    return true; // Can move down
   }
 }
