d12d18694e4e40f9df584a606014f809c5f6f955
Lucas <rolu22fh@student.ju.se>
2023-08-10 07:31:09 +0200

    a

--- tetris.js
+++ tetris.js
@@ -105,7 +105,7 @@ function updateGame() {
       if (platformSpawnTimer >= PLATFORM_SPAWN_INTERVAL) {
         platformSpawnTimer = 0;
         platform.x = random(0, COLS - PLATFORM_WIDTH);
-        platform.y = lastPlacedRow - 3; // Spawn above the last placed piece
+        platform.y = lastPlacedRow - 3;
       }
     }
 
